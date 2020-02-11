63ef939772ec21003b99d80216bacbadac921f58
Loarlam <messiah02@mail.ru>
2020-02-11 21:43:47 +0300

    Add project files.

---
+++ Addition_task/Dictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Addition_task
+{
+    class Dictionary
+    {
+        string[] _rus = new string[5];
+        string[] _eng = new string[5];
+        string[] _ua = new string[5];
+
+        public Dictionary()
+        {
+            _rus[0] = "книга"; 
+            _rus[1] = "ручка"; 
+            _rus[2] = "солнце"; 
+            _rus[3] = "яблоко"; 
+            _rus[4] = "стол";
+            _eng[0] = "book";
+            _eng[1] = "pen";
+            _eng[2] = "sun";
+            _eng[3] = "apple";
+            _eng[4] = "table";
+            _ua[0] = _rus[0];
+            _ua[1] = _rus[1];
+            _ua[2] = "сонце";
+            _ua[3] = "яблуко";
+            _ua[4] = "стiл";
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                for (int i = 0; i < _rus.Length; i++)
+                    if (_rus[i] == index)
+                        return _rus[i] + " - " + _eng[i];
+
+                return string.Format("{0} - нет перевода для этого слова.", index);
+            }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < _rus.Length)
+                    return _rus[index] + " - " + _eng[index];
+                else
+                    return "Попытка обращения за пределы массива.";
+            }
+        }
+    }
+}
---
+++ Addition_task/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Расширьте пример 5 (русско-английский словарь) еще и украинским словарем. 
+ Реализуйте возможность поиска не только по ключевым русским словам и словам на остальных языках. 
+ */
+using System;
+
+namespace Addition_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Строка ниже для возможности отображения украинских символов в консольке
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+            Dictionary dictionary = new Dictionary();
+
+            Console.WriteLine(dictionary["книга"]);
+            Console.WriteLine(dictionary["дом"]);
+            Console.WriteLine(dictionary["ручка"]);
+            Console.WriteLine(dictionary["стол"]);
+            Console.WriteLine(dictionary["карандаш"]);
+            Console.WriteLine(dictionary["яблоко"]);
+            Console.WriteLine(dictionary["солнце"]);
+            Console.WriteLine(dictionary["хата"]);
+            Console.WriteLine(dictionary["стiл"]);
+            Console.WriteLine(dictionary["олівець"]);
+            Console.WriteLine(dictionary["яблуко"]);
+            Console.WriteLine(dictionary["сонце"]);
+            Console.WriteLine("book");
+            Console.WriteLine("pen");
+            Console.WriteLine("sun");
+            Console.WriteLine("apple");
+            Console.WriteLine("table");
+
+            Console.WriteLine(new string('-', 30));
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(dictionary[i]);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
